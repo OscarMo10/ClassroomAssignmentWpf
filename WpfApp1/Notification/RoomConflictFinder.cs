a1d839ee95e2bc48fa48aff548dc1bdf2b1ff4df
Oscar Montoya <omontoya@unomaha.edu>
2018-04-03 18:16:38 -0500

    Added tests.

--- WpfApp1/Notification/RoomConflictFinder.cs
+++ WpfApp1/Notification/RoomConflictFinder.cs
@@ -9,19 +9,37 @@ namespace ClassroomAssignmentWpf.Notification
 {
     public class RoomConflictFinder
     {
+
+        /// <summary>
+        /// Return list of conflicts involving <paramref name="course"/> and the rest of the courses in the repo.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public List<Conflict> ConflictsInvolvingCourse(Course course)
         {
             return new List<Conflict>();
         }
 
+        /// <summary>
+        /// Finds conflicts involving the <paramref name="courses"/> and the rest of the courses in the CourseRepo
+        /// </summary>
+        /// <param name="courses"></param>
+        /// <returns></returns>
         public List<Conflict> ConflictsInvolvingCourses(List<Course> courses)
         {
             return new List<Conflict>();
         }
 
+
+        /// <summary>
+        /// Return conflicts solely among the <paramref name="courses"/>
+        /// </summary>
+        /// <param name="courses"></param>
+        /// <returns></returns>
         public List<Conflict> ConflictsAmongCourses(List<Course> courses)
         {
             return new List<Conflict>();
         }
+
     }
 }

80999ded07f7b5f6c399bdcc012a445c0f7548aa
Oscar Montoya <omontoya@unomaha.edu>
2018-04-06 12:36:46 -0500

    Deleted extraneous file

--- module1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import fileinput
-
-with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace(text_to_search, replacement_text), end='')
